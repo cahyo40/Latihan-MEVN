9901e30761ac99aeea33e7f96a1cf3db60c2790a
c4hyo <mdcnugroho@gmail.com>
2024-10-23 10:48:02 +0700

    #4

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const express = require("express");
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+
 
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
+
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hello World'
+    });
+});
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error.message);
+    });
---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Test</h1>
+  <p>{{ data.message }}</p>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue'
+
+
+const data = ref(null);
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/');
+    data.value = response.data;
+    console.log(data.value);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+onMounted(async () => {
+  fetchData();
+});
+
+</script>
